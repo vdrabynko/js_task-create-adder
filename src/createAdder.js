4507305727cfd6c14b0302aab147352ee398887c
vpolets <vpolets@gmail.com>
2019-06-21 16:18:08 +0300

    add task and tests

---
+++ src/createAdder.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Write a function (factory) creating a function (device) returning the sum of
+ * initialValue and all the arguments of current and all previous calls. By
+ * default initial value is 0.
+ *
+ * For example:
+ *
+ * const adder1 = createAdder()
+ * console.log(adder1()); // 0
+ * console.log(adder1(10, 20)); // 30 = 0 + (10 + 20)
+ * console.log(adder1(30, 40)); // 100 = 0 + (10 + 20) + (30 + 40)
+ *
+ * const adder2 = createAdder(100);
+ * console.log(adder2(10)); // 110 = 100 + 10
+ * console.log(adder2(20, 30, 40)); 200 = 100 + 10 + (20 + 30 + 40)
+ * console.log(adder2()); 200 = the same as previous
+ *
+ * @param {number} initialValue
+ *
+ * @return {number}
+ */
+function createAdder(initialValue = 0) {
+  // write code here
+}
+
+module.exports = createAdder;
